0024dc5263bfa8ac924a9e2f6e3c54ab24f2bf94
PABLO LOZANO <117815126+XLozax12@users.noreply.github.com>
2023-05-23 10:07:15 +0200

    arreglo categorias y joyas

--- main.js
+++ main.js
@@ -2,32 +2,32 @@ indice();
 document.addEventListener("DOMContentLoaded", traerProductos);
 const contenedor = document.querySelector("#contenedor");
 let respuesta;
-let nproductos=8;
+let nproductos = 8;
 
 let carrito;
 crearCarrito();
 
 async function crearCarrito() {
-  await fetch('https://fakestoreapi.com/carts',{
-            method:"POST",
-            body:JSON.stringify(
-                {
-                    userId:1,
-                    date:2023-02-03,
-                    products:[{productId:1,quantity:1}]
-                }
-            )
-        })
-            .then(res=>res.json())
-            .then(json=>carrito=json)
-console.log(carrito);
-  
+  await fetch('https://fakestoreapi.com/carts', {
+    method: "POST",
+    body: JSON.stringify(
+      {
+        userId: 1,
+        date: 2023 - 02 - 03,
+        products: [{ productId: 1, quantity: 1 }]
+      }
+    )
+  })
+    .then(res => res.json())
+    .then(json => carrito = json)
+
+
 }
 
 // traer los productos
 async function traerProductos() {
   console.log("traer prodcutoss " + nproductos);
-  const url = "https://fakestoreapi.com/products?limit="+nproductos;
+  const url = "https://fakestoreapi.com/products?limit=" + nproductos;
 
   try {
     // await bloquea la ejecucion del codigo y hasta que no se resuelva no va  a ejecutarse el resultado
@@ -45,10 +45,10 @@ async function traerProductos() {
 function pintarProductos(productos) {
   document.getElementById("contenedor").innerHTML = "";
   console.log(productos);
-  
+
   productos.forEach((prod) => {
     const { id, title, price, image } = prod;
-    document.getElementById("contenedor").innerHTML +=  `
+    document.getElementById("contenedor").innerHTML += `
     <div class="col-3">
         <div class="card" style="width: 18rem;">    
   <img class="card-img-top" src="${image}" alt="Card image cap">
@@ -64,69 +64,51 @@ function pintarProductos(productos) {
         
         `;
   });
-      
+
 }
 
-function añadirCarrito(id){
+function añadirCarrito(id) {
   console.log(id);
-  // console.log(carrito);
-  fetch('https://fakestoreapi.com/carts/'+carrito.id,{
-            method:"PUT",
-            body:JSON.stringify(
-                {
-                    userId:5,
-                    date:2019-12-10,
-                    products:[{productId:id,quantity:1}]
-                }
-            )
-        })
-            .then(res=>res.json())
-            .then(json=>console.log(json))
-  
+  console.log(carrito);
+  fetch('https://fakestoreapi.com/carts/' + carrito.id, {
+    method: "PUT",
+    body: JSON.stringify(
+      {
+        userId: 5,
+        date: 2019 - 12 - 10,
+        products: [{ productId: id, quantity: 1 }]
+      }
+    )
+  })
+    .then(res => res.json())
+    .then(json => console.log(json))
+
 }
 
-
-// Categorias
-// en vez de desc pongo asc pa mostrar ascendente
-fetch("https://fakestoreapi.com/products/categories?sort=asc")
-  .then((res) => res.json())
-  .then((json) => (categoriasjson = json));
-
-function muestraCategorias(j) {
-  for (i = 0; i < j.length; i++) {
-    // $("#contenedor").append(j[i]);
-    $("#list").append("<li><h3>" + j[i] + "</h3></li>");
-  }
-}
 
 // capas
 
 document.getElementById("indice").addEventListener("click", indice);
 
 function indice() {
-  document.getElementById("categorias").style.display="none";
+  document.getElementById("categorias").innerHTML = "";
+  document.getElementById("contenedor").innerHTML = "";
   document.getElementById("contenedor").innerHTML =
-    "<h1>Soy la empresa de lozashop</h1>";
+    "<h1>Estas en el Inicio de la tienda</h1>";
 }
 // productos
 document.getElementById("Productos").addEventListener("click", function () {
   pintarProductos(respuesta);
 });
 
-// categorias
-document.getElementById("Categorias").addEventListener("click", function () {
-  document.getElementById("contenedor").innerHTML = "";
-  document.getElementById("categorias").style.display="block";
-   
 
-
-  // Categoria Electronics
-  fetch('https://fakestoreapi.com/products/category/electronics')
+// Categoria Electronics
+fetch('https://fakestoreapi.com/products/category/electronics')
   .then(res => res.json())
   .then(json => categoria1 = json)
 function muestraCategoria(j) {
   for (i = 0; i < j.length; i++) {
-      contenedor.innerHTML += `
+    document.getElementById("categorias").innerHTML += `
       <div class="col-2">
           <div class="card" style="width: 15rem;">    
               <img class="card-img-top" src="${j[i].image}" alt="Card image cap" with="200px" height="300px">
@@ -146,22 +128,21 @@ function muestraCategoria(j) {
 }
 
 // Capa electronics
-    //  Electronics
-    document.getElementById("electronica").addEventListener("click", function () {
-      // limpiarBody();
-      document.getElementById("categorias").innerHTML="";
-      document.getElementById('categorias').style = 'display:block'
-      // console.log(categoriasjson);
-      muestraCategoria(categoria1);
-  });
+//  Electronics
+document.getElementById("electronica").addEventListener("click", function () {
+  // limpiarBody();
+  document.getElementById("contenedor").innerHTML = "";
+  document.getElementById("categorias").innerHTML = "";
+  muestraCategoria(categoria1);
+});
 
-  // Categoria joya
-  fetch('https://fakestoreapi.com/products/category/jewelery')
+// Categoria joya
+fetch('https://fakestoreapi.com/products/category/jewelery')
   .then(res => res.json())
   .then(json => categoria2 = json)
 function muestraCategoria(j) {
   for (i = 0; i < j.length; i++) {
-      contenedor.innerHTML += `
+    contenedor.innerHTML += `
       <div class="col-2">
           <div class="card" style="width: 15rem;">    
               <img class="card-img-top" src="${j[i].image}" alt="Card image cap" with="200px" height="300px">
@@ -169,7 +150,6 @@ function muestraCategoria(j) {
                   <h5 class="card-title">${j[i].title}</h5>
                   <p class="card-subtitle mb-2 text-muted">Precio: ${j[i].price}$</p>
                   <h5 class="card-title">${j[i].category}</h5>
-
 
                   <button onclick="" class="btn btn-primary"> Ver </button>
                   <a href="#" class="btn btn-primary">Comprar</a>
@@ -181,42 +161,25 @@ function muestraCategoria(j) {
 }
 
 // Capa joya
-    //  joyas
-    document.getElementById("joyas").addEventListener("click", function () {
-      // limpiarBody();
-      document.getElementById("categorias").innerHTML="";
-      document.getElementById('categorias').style = 'display:block'
-      // console.log(categoriasjson);
-      muestraCategoria(categoria2);
-  });
+//  joyas
+document.getElementById("joyas").addEventListener("click", function () {
 
-
-
-
-
-
-  // limpiarBody();
-  // console.log(categoriasjson);
-  muestraCategorias(categoriasjson);
+  document.getElementById("categorias").innerHTML = "";
+  document.getElementById("contenedor").innerHTML = "";
+  muestraCategoria(categoria2);
 });
 
 
-// // carrito
-// document.getElementById("carrito").addEventListener("click", function () {
-//   document.getElementById("contenedor").innerHTML = "";
-//   document.getElementById("carrito").style.display="block";
-//   fetch('https://fakestoreapi.com/carts')
-//             .then(res=>res.json())
-//             .then(json=>console.log(json))
-
-// });
 
 
-
-
-
-
-
+// carrito
+document.getElementById("carrito").addEventListener("click", function () {
+  document.getElementById("contenedor").innerHTML = "";
+  document.getElementById("carrito").style.display = "block";
+  fetch('https://fakestoreapi.com/carts')
+    .then(res => res.json())
+    .then(json => console.log(json))
+});
 
 // Scroll infinito
 $(window).scroll(function () {
@@ -225,11 +188,11 @@ $(window).scroll(function () {
     $(window).scrollTop() + $(window).height() >=
     $(document).height() - 100
   ) {
-    nproductos=nproductos+8;
+    nproductos = nproductos + 8;
     alert("Esta cargando los productos espere porfavor");
-    
+
     console.log("scroll productos " + nproductos);
 
-   traerProductos();
+    traerProductos();
   }
 });
