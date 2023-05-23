df4673883a5cbf75c25079b3b24fc95bc78bfb41
PABLO LOZANO <117815126+XLozax12@users.noreply.github.com>
2023-05-23 09:25:16 +0200

    proyecto
    e

--- main.js
+++ main.js
@@ -4,6 +4,25 @@ const contenedor = document.querySelector("#contenedor");
 let respuesta;
 let nproductos=8;
 
+let carrito;
+crearCarrito();
+
+async function crearCarrito() {
+  await fetch('https://fakestoreapi.com/carts',{
+            method:"POST",
+            body:JSON.stringify(
+                {
+                    userId:1,
+                    date:2023-02-03,
+                    products:[{productId:1,quantity:1}]
+                }
+            )
+        })
+            .then(res=>res.json())
+            .then(json=>carrito=json)
+console.log(carrito);
+  
+}
 
 // traer los productos
 async function traerProductos() {
@@ -29,7 +48,7 @@ function pintarProductos(productos) {
   
   productos.forEach((prod) => {
     const { id, title, price, image } = prod;
-    contenedor.innerHTML += `
+    document.getElementById("contenedor").innerHTML +=  `
     <div class="col-3">
         <div class="card" style="width: 18rem;">    
   <img class="card-img-top" src="${image}" alt="Card image cap">
@@ -37,7 +56,8 @@ function pintarProductos(productos) {
     <h5 class="card-title">${title}</h5>
     <p class="card-subtitle mb-2 text-muted">Precio: ${price}</p>
 
-    <a href="#" class="btn btn-primary">Añadir al carrito</a>
+    <a href="#" onclick='añadirCarrito(${id})' class="btn btn-primary add-to-cart">Añadir al carrito</a>
+    
   </div>
 </div>      
 </div>      
@@ -46,6 +66,25 @@ function pintarProductos(productos) {
   });
       
 }
+
+function añadirCarrito(id){
+  console.log(id);
+  // console.log(carrito);
+  fetch('https://fakestoreapi.com/carts/'+carrito.id,{
+            method:"PUT",
+            body:JSON.stringify(
+                {
+                    userId:5,
+                    date:2019-12-10,
+                    products:[{productId:id,quantity:1}]
+                }
+            )
+        })
+            .then(res=>res.json())
+            .then(json=>console.log(json))
+  
+}
+
 
 // Categorias
 // en vez de desc pongo asc pa mostrar ascendente
@@ -78,11 +117,106 @@ document.getElementById("Productos").addEventListener("click", function () {
 document.getElementById("Categorias").addEventListener("click", function () {
   document.getElementById("contenedor").innerHTML = "";
   document.getElementById("categorias").style.display="block";
+   
+
+
+  // Categoria Electronics
+  fetch('https://fakestoreapi.com/products/category/electronics')
+  .then(res => res.json())
+  .then(json => categoria1 = json)
+function muestraCategoria(j) {
+  for (i = 0; i < j.length; i++) {
+      contenedor.innerHTML += `
+      <div class="col-2">
+          <div class="card" style="width: 15rem;">    
+              <img class="card-img-top" src="${j[i].image}" alt="Card image cap" with="200px" height="300px">
+              <div class="card-body">
+                  <h5 class="card-title">${j[i].title}</h5>
+                  <p class="card-subtitle mb-2 text-muted">Precio: ${j[i].price}$</p>
+                  <h5 class="card-title">${j[i].category}</h5>
+
+
+                  <button onclick="" class="btn btn-primary"> Ver </button>
+                  <a href="#" class="btn btn-primary">Comprar</a>
+              </div>
+          </div> 
+      </div> 
+      `;
+  }
+}
+
+// Capa electronics
+    //  Electronics
+    document.getElementById("electronica").addEventListener("click", function () {
+      // limpiarBody();
+      document.getElementById("categorias").innerHTML="";
+      document.getElementById('categorias').style = 'display:block'
+      // console.log(categoriasjson);
+      muestraCategoria(categoria1);
+  });
+
+  // Categoria joya
+  fetch('https://fakestoreapi.com/products/category/jewelery')
+  .then(res => res.json())
+  .then(json => categoria2 = json)
+function muestraCategoria(j) {
+  for (i = 0; i < j.length; i++) {
+      contenedor.innerHTML += `
+      <div class="col-2">
+          <div class="card" style="width: 15rem;">    
+              <img class="card-img-top" src="${j[i].image}" alt="Card image cap" with="200px" height="300px">
+              <div class="card-body">
+                  <h5 class="card-title">${j[i].title}</h5>
+                  <p class="card-subtitle mb-2 text-muted">Precio: ${j[i].price}$</p>
+                  <h5 class="card-title">${j[i].category}</h5>
+
+
+                  <button onclick="" class="btn btn-primary"> Ver </button>
+                  <a href="#" class="btn btn-primary">Comprar</a>
+              </div>
+          </div> 
+      </div> 
+      `;
+  }
+}
+
+// Capa joya
+    //  joyas
+    document.getElementById("joyas").addEventListener("click", function () {
+      // limpiarBody();
+      document.getElementById("categorias").innerHTML="";
+      document.getElementById('categorias').style = 'display:block'
+      // console.log(categoriasjson);
+      muestraCategoria(categoria2);
+  });
+
+
+
+
+
 
   // limpiarBody();
   // console.log(categoriasjson);
   muestraCategorias(categoriasjson);
 });
+
+
+// // carrito
+// document.getElementById("carrito").addEventListener("click", function () {
+//   document.getElementById("contenedor").innerHTML = "";
+//   document.getElementById("carrito").style.display="block";
+//   fetch('https://fakestoreapi.com/carts')
+//             .then(res=>res.json())
+//             .then(json=>console.log(json))
+
+// });
+
+
+
+
+
+
+
 
 // Scroll infinito
 $(window).scroll(function () {
