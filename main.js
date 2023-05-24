83a5101658e26cf523bdffc08033338c456eb07c
PABLO LOZANO <117815126+XLozax12@users.noreply.github.com>
2023-05-24 14:26:44 +0200

    carrito y inspeccionar hecho

--- main.js
+++ main.js
@@ -55,8 +55,22 @@ function pintarProductos(productos) {
   <div class="card-body">
     <h5 class="card-title">${title}</h5>
     <p class="card-subtitle mb-2 text-muted">Precio: ${price}</p>
+      
+
+        <div id="modal${id}" class="modal">
+          <h2>Modal</h2>
+          <img width="150" src="${image}" alt="Card image cap">
+		  <h5 >${title}</h5>
+		  <p>Precio: ${price}</p>
+          <button onclick='cerrar(${id})' class="close">Close</button> 
+		  <a href="#" onclick='añadirCarrito(${id})' class="btn btn-primary add-to-cart">Añadir al carrito</a>		  
+        </div>
+        <a href="#" id="${id}" onclick='abrir(${id})' class="btn btn-primary add-to-cart">ver</a>
+
+
 
     <a href="#" onclick='añadirCarrito(${id})' class="btn btn-primary add-to-cart">Añadir al carrito</a>
+    
     
   </div>
 </div>      
@@ -67,21 +81,22 @@ function pintarProductos(productos) {
 
 }
 
+
+function abrir(id){
+  $("#modal"+id).show();
+}
+
+function cerrar(id){
+  $("#modal"+id).hide();
+}
+
 function añadirCarrito(id) {
   console.log(id);
-  console.log(carrito);
-  fetch('https://fakestoreapi.com/carts/' + carrito.id, {
-    method: "PUT",
-    body: JSON.stringify(
-      {
-        userId: 5,
-        date: 2019 - 12 - 10,
-        products: [{ productId: id, quantity: 1 }]
-      }
-    )
-  })
-    .then(res => res.json())
-    .then(json => console.log(json))
+  
+  fetch('https://fakestoreapi.com/products/'+id)
+            .then(res=>res.json())
+            .then(json=>alert("El producto " +json.title+" se ha añadido al carrito " ))
+  
 
 }
 
@@ -173,13 +188,13 @@ document.getElementById("joyas").addEventListener("click", function () {
 
 
 // carrito
-document.getElementById("carrito").addEventListener("click", function () {
-  document.getElementById("contenedor").innerHTML = "";
-  document.getElementById("carrito").style.display = "block";
-  fetch('https://fakestoreapi.com/carts')
-    .then(res => res.json())
-    .then(json => console.log(json))
-});
+// document.getElementById("carrito").addEventListener("click", function () {
+//   document.getElementById("contenedor").innerHTML = "";
+//   document.getElementById("carrito").style.display = "block";
+//   fetch('https://fakestoreapi.com/carts')
+//     .then(res => res.json())
+//     .then(json => console.log(json))
+// });
 
 // Scroll infinito
 $(window).scroll(function () {
